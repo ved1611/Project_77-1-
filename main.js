d3f26dc2726588d09c4c7bdafc4b2fb60944fa3b
ved1611 <107996716+ved1611@users.noreply.github.com>
2023-07-04 10:43:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+ var result =  first_name.concat(last_name);
+ console.log(result);
+ var a = 10;
+ var b = 5;
+ var add = a + b;
+ console.log(add);
